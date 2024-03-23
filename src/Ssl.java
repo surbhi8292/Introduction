0e9a2c13c0a8003dcf0463808939b9ea72237f72
surbhi8292 <163147580+surbhi8292@users.noreply.github.com>
2024-03-23 16:41:42 +0530

    broken link

--- src/Ssl.java
+++ src/Ssl.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
 		import org.openqa.selenium.WebDriver;
 		import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,10 +13,15 @@ import org.openqa.selenium.Capabilities;
 		import org.openqa.selenium.remote.DesiredCapabilities;
 public class Ssl {
 
-	public static void main(String[] args) {
-
-
-
+	public static void main(String[] args) throws MalformedURLException, IOException {
+WebDriver driver= new ChromeDriver();
+driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+String url=driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+HttpURLConnection Conn= (HttpURLConnection)new URL(url).openConnection();
+Conn.setRequestMethod("HEAD");
+Conn.connect();
+int responseCode= Conn.getResponseCode();
+System.out.println(responseCode);
 
 
 		}
